e311b0b232c4c874c07c7ae8f14c14462a8b3582
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-08-05 15:49:06 +0200

    semantic poprawka

--- scripts.js
+++ scripts.js
@@ -82,7 +82,7 @@ menuButton.addEventListener('click', () => {
 //wysuwane menu główne
 let burger = document.querySelector(".burger");
 burger.addEventListener('click', function(){
-    this.classList.toggle('clicked');
+    this.classList.toggle('clickedBurger');
     document.querySelector('.menu').classList.toggle('showed');
 });
 
@@ -91,7 +91,7 @@ let menuOptions = document.querySelectorAll('.menu > ul >li');
 for (const option of menuOptions) {
     option.addEventListener('click', function(){
         document.querySelector('.menu').classList.toggle('showed');
-        burger.classList.toggle('clicked');
+        burger.classList.toggle('clickedBurger');
         document.title="Weronika Wójcik | "+this.innerHTML;
         document.querySelector('.tittle').innerHTML=this.innerHTML;
     })
