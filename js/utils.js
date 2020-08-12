e5ca69197ec3bfe7bfbd07f995c7baae255a245b
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-08-12 23:27:48 +0200

    jeszcze bardziej poprawione getPostion

--- js/utils.js
+++ js/utils.js
@@ -4,6 +4,7 @@
  */
 function getPosition(element) {
     var top = 0, left = 0;
+    var scrollTop = 0, scrollLeft = 0;
     var h = element.clientHeight / 2, w = element.clientWidth / 2;
     var baseElement = element;
 
@@ -15,10 +16,19 @@ function getPosition(element) {
         element = element.offsetParent;
     } while (element);
 
+    element = baseElement;
+
+    do {
+        scrollTop -= element.parentElement.scrollTop || 0;
+        scrollLeft -= element.parentElement.scrollLeft || 0;
+        element = element.parentElement;
+    } while (element.parentElement);
+
+
 
     return {
-        top: top + h,
-        left: left + w
+        top: top + scrollTop + h,
+        left: left + scrollLeft + w
     };
 }
 
