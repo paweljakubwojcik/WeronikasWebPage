0251e81f09661f622a7f4fd79cc66f77c91048d9
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-09-19 23:10:59 +0200

    ładowanie obrazków z servera

--- js/galeria.js
+++ js/galeria.js
@@ -1,78 +1,107 @@
 import getPosition from './utils';
 import makeTilt from './tilt';
 import Tween from 'gsap';
-import dataObject from './pictures'
-let data = dataObject.data;
+
+
 
 let images, modal, fullSizeImage, text, folders;
 
 export default {
     init() {
 
-        insertPictures(data)
+        fetch('https://cms-for-w-w.herokuapp.com/folders')
+            .then(response => response.json())
+            .then(data => {
+                insertPictures(data)
 
-        images = document.querySelectorAll('.obrazek img');
-        modal = document.querySelector('.modal');
-        fullSizeImage = document.querySelector('.modal img');
-       // text = document.querySelector('.modal p');
-        folders = document.querySelectorAll('.folder')
 
-        makeTilt('.obrazek img');
+                initFolders();
 
-        let delay = .3;
-
-        for (const image of images) {
-            delay += 0.05;
-            image.addEventListener('click', () => {
-                let path = image.getAttribute('data-full-size');
-                modal.classList.add('open');
-                fullSizeImage.src = path;
-                fullSizeImage.setAttribute('data-path', path)
-                //text.innerHTML = image.getAttribute('data-text');
             })
-        }
-
-        modal.addEventListener('click', (e) => {
-            if (e.target.classList.contains('modal'))
-                modal.classList.remove('open');
-            else if (e.target.localName == 'img') {
-                if (getPosition(fullSizeImage).left - e.x < 0)
-                    nextImage(true);
-                else
-                    nextImage(false);
-            }
-        })
-
-        initFolders();
+            .catch(err => console.log(err));
     }
+}
+
+
+//inicjalizuje zachowania obrazków
+function initPictures() {
+    images = document.querySelectorAll('.obrazek img');
+    modal = document.querySelector('.modal');
+    fullSizeImage = document.querySelector('.modal img');
+    // text = document.querySelector('.modal p');
+
+
+    makeTilt('.obrazek img');
+
+    let delay = .3;
+
+    for (const image of images) {
+        delay += 0.05;
+        image.addEventListener('click', () => {
+            let id = image.getAttribute('data-id');
+            modal.classList.add('open');
+            fullSizeImage.src = image.src;
+            fullSizeImage.setAttribute('data-id', id)
+            //text.innerHTML = image.getAttribute('data-text');
+        })
+    }
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.classList.contains('modal'))
+            modal.classList.remove('open');
+        else if (e.target.localName == 'img') {
+            if (getPosition(fullSizeImage).left - e.x < 0)
+                nextImage(true);
+            else
+                nextImage(false);
+        }
+    })
+
 }
 
 
 
 function insertPictures(data) {
+
     let galeria = document.querySelector('.Galeria')
     data.forEach(folder => {
-        
+
         let folderElement = document.createElement('div')
         folderElement.classList.add('folder')
         folderElement.classList.add('collapsed')
 
         let p = document.createElement('p')
-        p.innerHTML = folder.name;
+        p.innerHTML = folder.title;
         folderElement.appendChild(p)
 
-        folder.pictures.forEach(picture => {
-            let obrazekElement = document.createElement('div')
-            obrazekElement.classList.add('obrazek')
+        let index = 0;
 
-            let img = document.createElement('img')
-            img.setAttribute('src', picture.preview)
-            img.setAttribute('data-alt', picture.alt)
-            img.setAttribute('data-text', picture.name)
-            img.setAttribute('data-full-size', picture.src)
+        folder.items.forEach(picture => {
 
-            obrazekElement.appendChild(img)
-            folderElement.appendChild(obrazekElement)
+            fetch(`https://cms-for-w-w.herokuapp.com/folders/${picture}`)
+                .then(response => response.json())
+                .then(pic => {
+
+                    index++;
+                    let obrazekElement = document.createElement('div')
+                    obrazekElement.classList.add('obrazek')
+
+
+                    let img = document.createElement('img')
+                    img.setAttribute('src', convertImage(pic.img.data))
+                    img.setAttribute('data-alt', 'obrazek')
+                    img.setAttribute('data-text', pic.title)
+                    img.setAttribute('data-id', pic._id)
+
+                    obrazekElement.appendChild(img)
+                    folderElement.appendChild(obrazekElement)
+                    //na koniec
+                    if (index === folder.items.length)
+                        initPictures();
+                })
+                .catch(err => console.log(err));
+
+
         })
 
         galeria.appendChild(folderElement)
@@ -85,6 +114,7 @@ function insertPictures(data) {
  * inicjalizuje zachowanie folderów
  */
 function initFolders() {
+    folders = document.querySelectorAll('.folder')
     folders.forEach(folder => {
 
         // Tween.fromTo(folder,.4,{
@@ -124,15 +154,28 @@ function nextImage(isForward) {
 
     //find image which is being displayed
     let theImage = Array.from(images).filter((element) => {
-        return element.getAttribute('data-full-size') === fullSizeImage.getAttribute('data-path');
+        return element.getAttribute('data-id') === fullSizeImage.getAttribute('data-id');
     });
     //find the next or previous image
     let nextImage = forward ? theImage[0].parentElement.nextElementSibling : theImage[0].parentElement.previousElementSibling;
     if (nextImage) {
         //replace current image with found one
         nextImage = nextImage.firstElementChild;
-        fullSizeImage.src = nextImage.getAttribute('data-full-size');
-        fullSizeImage.setAttribute('data-path', nextImage.getAttribute('data-full-size'))
-       // text.innerHTML = nextImage.getAttribute('data-text');
+        fullSizeImage.src = nextImage.src;
+        fullSizeImage.setAttribute('data-id', nextImage.getAttribute('data-id'))
+        // text.innerHTML = nextImage.getAttribute('data-text');
     }
+}
+
+function convertImage(BufferData) {
+
+    let TYPED_ARRAY = new Uint8Array(BufferData);
+
+    const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+        return data + String.fromCharCode(byte);
+    }, '');
+
+    let base64String = btoa(STRING_CHAR);
+
+    return 'data:image/jpg;base64,' + base64String;
 }
