bd31fe47479b361344fa5390071768fff608957f
Paweł Wójcik <pawel.jakub.wojcik@gmail.com>
2020-10-25 21:33:22 +0100

    dodanie funkcji wysyłania emaili

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
 export const cmsBaseURL = 'https://cms-strapi-weronika-wojcik.herokuapp.com'
+//export const cmsBaseURL = 'http://localhost:1337'
--- js/email.js
+++ js/email.js
@@ -1,14 +1,67 @@
+import { cmsBaseURL } from './config'
+
+let form, button, name, email, message;
+
 export default {
     init: () => {
+        form = document.querySelector('.contact__form')
+        button = form.querySelector('button')
+        name = form.querySelector('#name')
+        email = form.querySelector('#email')
+        message = form.querySelector('#message')
 
-        const form = document.querySelector('.contact__form')
         form.addEventListener('submit', (e) => {
             e.preventDefault()
-            let name = form.querySelector('#name').value
-            let email = form.querySelector('#email').value
-            let message = form.querySelector('#message').value
-            console.log({ name, email, message })
+
+
+            let data = { name: name.value, email: email.value, message: message.value }
+            console.log({ data })
+
+            button.classList.add('loading')
+            button.innerHTML = 'Wysyłanie'
+            if (name.value.length === 0)
+                button.innerHTML = 'Proszę podac mi swojie imię'
+            if (email.value.length === 0)
+                button.innerHTML = 'Email jest potrzebny'
+            if (message.value.length === 0)
+                button.innerHTML = 'Nie wyśle pustej wiadomości'
+            if (button.innerHTML === 'Wysyłanie')
+                fetch(`${cmsBaseURL}/emails`, {
+                    method: 'POST', // or 'PUT'
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        onFetchEnd(data.succes)
+
+                    })
+                    .catch((error) => {
+                        onFetchEnd(false)
+                        console.error('Error:', error);
+                    });
+
+        })
+
+        message.addEventListener('input', () => {
+            if (message.value.length !== 0)
+                button.innerHTML = 'Wyślij'
         })
 
     }
+}
+
+const onFetchEnd = (succes) => {
+    button.classList.remove('loading')
+    button.blur()
+    if (succes) {
+        button.innerHTML = 'Wysłano'
+        name.value = ''
+        email.value = ''
+        message.value = ''
+    }
+    else
+        button.innerHTML = 'Coś poszło nie tak'
 }
